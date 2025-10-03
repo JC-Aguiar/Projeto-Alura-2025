67bb670258da048f67f48883c211f37f6674acad
joao.aguiar <joao.aguiar@ppware.com.br>
2025-10-03 13:05:49 -0300

    feat(course): creating `CourseService`
    This is part of the question n1 from the Alura's challenge

---
+++ src/main/java/br/com/alura/projeto/course/domain/CourseService.java
@@ -0,0 +1,52 @@
+package br.com.alura.projeto.course.domain;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class CourseService {
+
+    private final CourseRepository courseRepository;
+
+    @Autowired
+    public CourseService(CourseRepository courseRepository) {
+        this.courseRepository = courseRepository;
+    }
+
+    public Optional<Course> getById(Long id) {
+        if (id == null) throw new IllegalArgumentException(
+            "Invalid operation: couldn't find a Course record because we receive a null id value"
+        );
+        return courseRepository.findById(id);
+    }
+
+    public Course save(Course course) {
+        if (course == null) throw new IllegalArgumentException(
+            "Invalid operation: couldn't save a Course record because we receive a null register"
+        );
+        return courseRepository.save(course);
+    }
+
+    public List<Course> list() {
+        return courseRepository.findAll();
+    }
+
+    public List<Course> list(Example<Course> courseExample) {
+        return courseRepository.findAll(courseExample);
+    }
+
+    public Page<Course> list(Pageable pageable) {
+        return courseRepository.findAll(pageable);
+    }
+
+    public Page<Course> list(Example<Course> courseExample, Pageable pageable) {
+        return courseRepository.findAll(courseExample, pageable);
+    }
+
+}
