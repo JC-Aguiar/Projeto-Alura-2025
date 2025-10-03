9f3477dd12f3ac1e96a71051b39d4bcdd5afb791
joao.aguiar <joao.aguiar@ppware.com.br>
2025-10-03 20:36:33 -0300

    fix(dto): applying pattern in `SearchCourseFormDTO`, `course` attribute
    This is part of the question n1 from the Alura's challenge

--- src/main/java/br/com/alura/projeto/course/dto/SearchCourseFormDTO.java
+++ src/main/java/br/com/alura/projeto/course/dto/SearchCourseFormDTO.java
@@ -3,6 +3,7 @@ package br.com.alura.projeto.course.dto;
 import br.com.alura.projeto.course.domain.CourseStatusType;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import org.hibernate.validator.constraints.Length;
 
 import java.time.LocalDate;
@@ -10,10 +11,18 @@ import java.time.OffsetDateTime;
 import java.time.ZonedDateTime;
 
 public record SearchCourseFormDTO (
-    @Length(max = 256) String name,
-    @Length(max = 10) String code,
-    @Length(max = 300) String description,
-    @Length(max = 150) String instructorEmail,
+    @Length(max = 256)
+    String name,
+
+    @Length(max = 10)
+    @Pattern(regexp = "^[a-zA-Z]+(-[a-zA-Z]+)*$")
+    String code,
+
+    @Length(max = 300)
+    String description,
+
+    @Length(max = 150)
+    String instructorEmail,
     CourseStatusType status,
     OffsetDateTime inactivationDate) {
 
