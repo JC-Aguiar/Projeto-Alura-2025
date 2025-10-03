a4916ccd3592c19af939af9a494f71ac3fd9f051
joao.aguiar <joao.aguiar@ppware.com.br>
2025-10-03 13:03:52 -0300

    feat(course): creating `Course` entity and `CourseStatusType` enum
    This is part of the question n1 from the Alura's challenge

---
+++ src/main/java/br/com/alura/projeto/course/domain/Course.java
@@ -0,0 +1,44 @@
+package br.com.alura.projeto.course.domain;
+
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.OffsetDateTime;
+import java.time.ZonedDateTime;
+import java.util.Optional;
+
+@Data
+@Builder
+@Entity
+public class Course {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String code;
+
+    private String description;
+
+    private String instructorEmail;
+
+    @Builder.Default
+    @Enumerated(EnumType.STRING)
+    private CourseStatusType status = CourseStatusType.ACTIVE;
+
+    private OffsetDateTime inactivationDate;
+
+    @Deprecated
+    public Course() {}
+
+    public Optional<String> getDescription() {
+        return Optional.ofNullable(description);
+    }
+
+    public Optional<OffsetDateTime> getInactivationDate() {
+        return Optional.ofNullable(inactivationDate);
+    }
+}
---
+++ src/main/java/br/com/alura/projeto/course/domain/CourseStatusType.java
@@ -0,0 +1,24 @@
+package br.com.alura.projeto.course.domain;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+
+import java.util.Arrays;
+import java.util.Optional;
+
+public enum CourseStatusType {
+    ACTIVE,
+    INACTIVE;
+
+    @JsonCreator
+    public static CourseStatusType fromValue(String value) {
+        return identify(value).orElseThrow(
+            () -> new IllegalArgumentException("Unknown status: " + value)
+        );
+    }
+
+    public static Optional<CourseStatusType> identify(String value) {
+        return Arrays.stream(CourseStatusType.values())
+            .filter(type -> type.name().equalsIgnoreCase(value))
+            .findFirst();
+    }
+}
