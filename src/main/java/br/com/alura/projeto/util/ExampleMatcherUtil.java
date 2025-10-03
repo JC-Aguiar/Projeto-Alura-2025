a08bd136c82e28eb4e86ce7dcd22e70226f78c24
joao.aguiar <joao.aguiar@ppware.com.br>
2025-10-03 17:39:20 -0300

    feat(matcher): add `ExampleMatcherUtil` class for static common `ExampleMatcher`s
    This is part of the question n1 from the Alura's challenge

---
+++ src/main/java/br/com/alura/projeto/util/ExampleMatcherUtil.java
@@ -0,0 +1,26 @@
+package br.com.alura.projeto.util;
+
+import org.springframework.data.domain.ExampleMatcher;
+
+public class ExampleMatcherUtil {
+
+    /// Create a restrictive example-matcher. This means that all fields must match
+    /// in order to be considered acceptable.
+    public final static ExampleMatcher MATCHER_ALL = ExampleMatcher
+        .matchingAll()
+        .withIgnoreNullValues()
+        .withIgnoreCase();
+
+    /// Create a flexible example-matcher. This means that at last one field must match
+    /// in order to be considered acceptable.
+    public final static ExampleMatcher MATCHER_ANY = ExampleMatcher
+        .matchingAny()
+        .withIgnoreNullValues()
+        .withIgnoreCase();
+
+    /// Fabric method in order to create custom example-matchers base on the existing ones
+    public static ExampleMatcher createCustomMatcher(boolean matchAll) {
+        return matchAll ? MATCHER_ALL : MATCHER_ANY;
+    }
+
+}
