76484e04a496f36fbaf7de8e02c630e59aed2f42
joao.aguiar <joao.aguiar@ppware.com.br>
2025-10-03 13:40:14 -0300

    chore(course): add `@Repository` annotation in `CourseRepository`
    This is part of the question n1 from the Alura's challenge

--- src/main/java/br/com/alura/projeto/course/domain/CourseRepository.java
+++ src/main/java/br/com/alura/projeto/course/domain/CourseRepository.java
@@ -1,7 +1,9 @@
 package br.com.alura.projeto.course.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CourseRepository extends JpaRepository<Course, Long> {
 
 }
