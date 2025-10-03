74be1f96b06d5f3ec6cb9425a6e1eac6cb9da6ce
joao.aguiar <joao.aguiar@ppware.com.br>
2025-10-03 13:04:34 -0300

    feat(course): creating `CourseRepository`
    This is part of the question n1 from the Alura's challenge

---
+++ src/main/java/br/com/alura/projeto/course/domain/CourseRepository.java
@@ -0,0 +1,7 @@
+package br.com.alura.projeto.course.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CourseRepository extends JpaRepository<Course, Long> {
+
+}
