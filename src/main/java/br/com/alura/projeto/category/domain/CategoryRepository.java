8909dc6dc9c20d3515476c225705c4b442cdcd30
joao.aguiar <joao.aguiar@ppware.com.br>
2025-10-06 16:08:53 -0300

    build(ci): configuring amazon AWS RDS connection
    This means that all new native queries have been updated, in order to avoid possible case-sensitivity issues.
    Also, `ci.yml` has been change to include environment secrets inside `application.properties`

--- src/main/java/br/com/alura/projeto/category/domain/CategoryRepository.java
+++ src/main/java/br/com/alura/projeto/category/domain/CategoryRepository.java
@@ -11,24 +11,24 @@ public interface CategoryRepository extends JpaRepository<Category, Long> {
     boolean existsByCode(String code);
 
     @Query(nativeQuery = true, value = """
-        with records as (
-            select  category.name           as category,
-                    category.code           as categoryCode,
-                    category.color          as categoryColor,
-                    category.order          as categoryOrder,
-                    course.name             as course,
+        with Records as (
+            select  Category.name           as category,
+                    Category.code           as categoryCode,
+                    Category.color          as categoryColor,
+                    Category.order          as categoryOrder,
+                    Course.name             as course,
                     row_number() over (
-                        partition   by category.id
-                        order       by course.id
+                        partition   by Category.id
+                        order       by Course.id
                     )                       as rowNum
-            from    course
-            join    category
-            on      course.categoryId = category.id
-            where   course.status = 'ACTIVE'
+            from    Course
+            join    Category
+            on      Course.categoryId = Category.id
+            where   Course.status = 'ACTIVE'
         )
         select  *
-        from    records
-        where   records.rowNum <= :totalCoursesPerCategory
+        from    Records
+        where   Records.rowNum <= :totalCoursesPerCategory
     """)
     List<SimpleCategoryAndCourse> findSomeActiveCoursesWithCategory(int totalCoursesPerCategory);
 }
