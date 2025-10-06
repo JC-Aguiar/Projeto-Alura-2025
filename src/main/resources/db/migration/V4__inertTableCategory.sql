1b6af75fb084ce7751a8346c670c28169ce25f1d
joao.aguiar <joao.aguiar@ppware.com.br>
2025-10-06 02:18:40 -0300

    feat(sql): scripts to insert categories and courses in the base
    This is part of the question n3 from the Alura's challenge

---
+++ src/main/resources/db/migration/V4__inertTableCategory.sql
@@ -0,0 +1,35 @@
+-- ##################################################################
+-- POPULANDO A TABELA CATEGORY
+-- ##################################################################
+
+-- PROGRAMAÇÃO
+INSERT INTO category (name, code, color, `order`) VALUES
+('Programação', 'BACK-END', '#00C86F', 1);
+
+-- FRONT-END
+INSERT INTO category (name, code, color, `order`) VALUES
+('Front-End', 'FRONT-END', '#6BD1FF', 2);
+
+-- DATA SCIENCE
+INSERT INTO category (name, code, color, `order`) VALUES
+('Data Science', 'DATA-SCIENCE', '#9CD33B', 3);
+
+-- INTELIGÊNCIA ARTIFICIAL
+INSERT INTO category (name, code, color, `order`) VALUES
+('Inteligência Artificial', 'IA', '#7B71FF', 4);
+
+-- DEVOPS
+INSERT INTO category (name, code, color, `order`) VALUES
+('DevOps', 'DEVOPS', '#F16165', 5);
+
+-- UX & DESIGN
+INSERT INTO category (name, code, color, `order`) VALUES
+('UX & Design', 'UX-DESIGN', '#DC6EBE', 6);
+
+-- MOBILE
+INSERT INTO category (name, code, color, `order`) VALUES
+('Mobile', 'MOBILE', '#FFBA05', 7);
+
+-- MOBILE
+INSERT INTO category (name, code, color, `order`) VALUES
+('Inovação & Gestão', 'INOVA-GESTAO', '#FF8C2A', 8);
