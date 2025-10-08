5e6aee0eb61d1b3c9b90b7b37f97beaa06804d44
joao.aguiar <joao.aguiar@ppware.com.br>
2025-10-07 22:32:49 -0300

    feat(category): adding overall info in `CategoryDTO`
    This is part of the question n4 from the Alura's challenge

--- src/main/java/br/com/alura/projeto/category/dto/CategoryDTO.java
+++ src/main/java/br/com/alura/projeto/category/dto/CategoryDTO.java
@@ -6,4 +6,14 @@ public record CategoryDTO(Long id, String name, String code, String color, int o
     public CategoryDTO(Category category) {
         this(category.getId(), category.getName(), category.getCode(), category.getColor(), category.getOrder());
     }
+
+    public String getResume() {
+        return """
+            ID: %d
+            CODE: %s
+            COLOR: %s
+            ORDER: %d
+            """
+            .formatted(id, code, color, order);
+    }
 }
