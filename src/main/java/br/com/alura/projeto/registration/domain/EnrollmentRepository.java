74ae1fe6227e070c6a4bb45f1139be8d271727ea
joao.aguiar <joao.aguiar@ppware.com.br>
2025-10-08 19:50:48 -0300

    feat(registration): adding the new `EnrollmentRepository` to fetch registrations
    This is part of the question n5 from the Alura's challenge

---
+++ src/main/java/br/com/alura/projeto/registration/domain/EnrollmentRepository.java
@@ -0,0 +1,15 @@
+package br.com.alura.projeto.registration.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface EnrollmentRepository extends JpaRepository<Enrollment, Long> {
+
+    boolean existsByUserIdAndCourseId(Long userId, Long courseId);
+
+    Optional<Enrollment> findByUserIdAndCourseId(Long userId, Long courseId);
+
+}
