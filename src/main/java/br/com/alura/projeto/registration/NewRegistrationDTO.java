4f7e17144a9abdbd444f084426f00e2c52ea15af
joao.aguiar <joao.aguiar@ppware.com.br>
2025-10-08 19:43:59 -0300

    feat(registration): adding all args constructor in `NewRegistrationDTO`
    This is part of the question n5 from the Alura's challenge

--- src/main/java/br/com/alura/projeto/registration/NewRegistrationDTO.java
+++ src/main/java/br/com/alura/projeto/registration/NewRegistrationDTO.java
@@ -3,7 +3,9 @@ package br.com.alura.projeto.registration;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
 
+@AllArgsConstructor
 public class NewRegistrationDTO {
 
     @NotBlank
