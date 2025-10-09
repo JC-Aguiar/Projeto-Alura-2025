cd79ab4e7a32c1d75edb91a1f34172d03330c760
joao.aguiar <joao.aguiar@ppware.com.br>
2025-10-09 03:01:02 -0300

    feat(registration): creating projection to retrieve data
    This is part of the question n6 from the Alura's challenge

---
+++ src/main/java/br/com/alura/projeto/registration/projection/RegistrationReportItemConcept.java
@@ -0,0 +1,9 @@
+package br.com.alura.projeto.registration.projection;
+
+public interface RegistrationReportItemConcept {
+    String getCourseName();
+    String getCourseCode();
+    String getInstructorName();
+    String getInstructorEmail();
+    Long getTotalRegistrations();
+}
